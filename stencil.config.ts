73131465f9be4ab86be440c2e010d67c76d8b85e
oliver <oliver@bitcaster.de>
2022-03-23 14:55:37 +0100

    add sass

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,8 +1,32 @@
 import { Config } from '@stencil/core';
+import {sass} from "@stencil/sass";
 
 export const config: Config = {
   namespace: 'felix-core',
-
+  enableCache: true,
+  hashFileNames: false,
+  autoprefixCss: false,
+  minifyCss: true,
+  extras: {
+    // We need the following for IE11 and old Edge:
+    cssVarsShim: true,
+    dynamicImportShim: true,
+    // We don’t use shadow DOM so this is not needed:
+    shadowDomShim: false,
+    // Setting the below option to “true” will actually break Safari 10 support:
+    safari10: false,
+    // This is to tackle an Angular specific performance issue:
+    initializeNextTick: true,
+    // Don’t need any of these so setting them to “false”:
+    scriptDataOpts: false,
+    appendChildSlotFix: false,
+    cloneNodeFix: false,
+    slotChildNodesFix: false,
+  },
+  buildEs5: 'prod',
+  plugins: [
+    sass()
+  ],
   outputTargets: [
     {
       type: 'dist',
