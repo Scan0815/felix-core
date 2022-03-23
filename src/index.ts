fd0762b682814ff1f21bf51218a563efc3b6174e
oliver <oliver@bitcaster.de>
2022-03-23 21:21:23 +0100

    change ts settings

--- src/index.ts
+++ src/index.ts
@@ -14,15 +14,4 @@ export {SuccessToast,ErrorToast,LoggedOutToast} from './helpers/default-toasts';
 export {NotLoggedInAlert} from './helpers/default-alerts';
 export {IsLoggedInGuard,RouterNavigate,RouterCanNavGoBackThenGo,RouterErrorHandling,RouterGetUriParam} from './helpers/router.utils';
 
-//Services
-export {environment as coreEnv} from "./services/environment.service";
-export {AccountService} from "./services/account.service";
-export {StorageService} from "./services/storage.service";
-export {ToastService} from "./services/toast.service";
-export {AuthService} from "./services/auth.service";
-export {i18n} from "./services/i18n.service";
-export {RestService} from "./services/rest.service";
-export {ModalService} from "./services/modal.service";
-export {ModalMenuService} from "./services/modalMenu.service";
-
 export { Components, JSX } from './components';
