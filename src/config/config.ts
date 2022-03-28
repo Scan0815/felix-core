54250eb7ddf7d40073a5c74e78717098bcd98aaf
oliver <oliver@bitcaster.de>
2022-03-28 13:47:58 +0200

    fix setup

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,5 +2,6 @@ import {IEnv} from "../interfaces/env";
 import {SetupService} from "../services/setup.service";
 
 export const setupFelixCore = (env:IEnv) => {
+  console.log('setUpFelixCore',env);
   SetupService.init(env);
 }
--- src/services/setup.service.ts
+++ src/services/setup.service.ts
@@ -24,7 +24,6 @@ class SetupServiceController{
     win.Felix = {
       config: this._config
     };
-
     console.log('win.Felix',win.Felix);
   }
 
